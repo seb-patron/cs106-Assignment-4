1b617cbae0037469eb467f4cbe3ced98d3c8191c
S <s>
2014-11-29 01:55:09 -0600

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -8,8 +8,7 @@ import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
 	
-//	public static final int APPLICATION_WIDTH = 400;
-//	public static final int APPLICATION_HEIGHT = 600;
+
 
 	/** Resets the display so that only the scaffold appears */
 	public void reset() {
@@ -17,7 +16,7 @@ public class HangmanCanvas extends GCanvas {
 	}
 
 	public void drawCanvas() {
-//		GCanvas canvas = new GCanvas();
+		GCanvas faggot = new GCanvas();
 	}
 
 	/**
