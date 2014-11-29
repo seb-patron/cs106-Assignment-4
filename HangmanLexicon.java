e38da574e0895b53c56d554a357e35b95a15e26b
S <s>
2014-11-29 09:17:23 -0600

    {"type":"TI", "runs":"1"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -6,30 +6,82 @@
  */
 
 import acm.util.*;
+import java.io.*;
+import java.util.*;
 
 public class HangmanLexicon {
-	
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
-/** Returns the number of words in the lexicon. */
+	private int wordCount = 0;
+	
+	private int MAX_SIZE = 500;
+	
+	//public String line;
+
+	String [] wordList = new String [MAX_SIZE];
+
+	/** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return wordCount;
 	}
 
-/** Returns the word at the specified index. */
-	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
+	public String getWord() {
+		String result;
+		int x = rgen.nextInt(0, wordCount);
+		result = wordList[x];
+		return result;
+		
+
+	}
+
+	private BufferedReader openFile() {
+		BufferedReader rd = null;
+		
+		while (rd == null) {
+			try {
+				rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+				while (true) {
+					String line = rd.readLine();
+					if (line == null) break; //breaks when lines all read
+					wordList[wordCount++] = line;
+				}
+			} catch (IOException ex) {
+			//	println("Bad file");
+			}
 		}
-	};
+		return rd;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///** Returns the word at the specified index. */
+//public String getWord(int index) {
+//	switch (index) {
+//		case 0: return "BUOY";
+//		case 1: return "COMPUTER";
+//		case 2: return "CONNOISSEUR";
+//		case 3: return "DEHYDRATE";
+//		case 4: return "FUZZY";
+//		case 5: return "HUBBUB";
+//		case 6: return "KEYHOLE";
+//		case 7: return "QUAGMIRE";
+//		case 8: return "SLITHER";
+//		case 9: return "ZIRCON";
+//		default: throw new ErrorException("getWord: Illegal index");
+//	}
+//}
