19791bf02cd6f84a751c82383b39b315659a7246
S <s>
2014-11-29 09:35:08 -0600

    {"type":"TI", "runs":"2"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -35,16 +35,14 @@ public class HangmanLexicon {
 
 	private BufferedReader openFile() {
 		BufferedReader rd = null;
-		int i = 0;
+		
 		while (rd == null) {
 			try {
 				rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
 				while (true) {
 					String line = rd.readLine();
 					if (line == null) break; //breaks when lines all read
-					wordList[i] = line;
-					i++;
-					wordCount++;
+					wordList[wordCount++] = line;
 				}
 			} catch (IOException ex) {
 			//	println("Bad file");
