7dca013f5468255523a47630e7eb33c68843be26
S <s>
2014-11-29 22:59:09 -0600

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -160,7 +160,7 @@ public class Hangman extends ConsoleProgram {
 		//and deincrements guess counter
 		if (word.indexOf(ch) == -1){
 			println("Sorry, but there are no " + ch + "'s in the word.");
-			guessCounter--;
+			guessCounter = 7;
 			canvas.noteIncorrectGuess(guessCounter);
 			
 		}
