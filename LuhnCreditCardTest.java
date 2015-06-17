d0de1b4253136a5900e7ce2de4557279ec20b957
S <s>
2015-06-16 18:37:21 -0700

    {"type":"TI", "runs":"1"}

--- LuhnCreditCardTest.java
+++ LuhnCreditCardTest.java
@@ -1,19 +1,19 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 import acm.program.ConsoleProgram;
 
 public class LuhnCreditCardTest extends ConsoleProgram{
 
-//	static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 	
-	public void run() {
-		println("Hey");
-	}
+//	public void run() {
+//		println("Hey");
+//	}
 	
-//	public static void main(String[] args) {
-//		System.out.print("Enter an interger");
-//		int x = sc.nextInt();
-//		System.out.println(x);
-//    }
+	public static void main(String[] args) {
+		System.out.print("Enter an interger");
+		int x = sc.nextInt();
+		System.out.println(x);
+    }
 	
 }
